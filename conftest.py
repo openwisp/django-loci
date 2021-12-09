b2aa8f791e64421b68e8e2ff1dc6ebf4c33d133c
Gagan Deep <pandafy.dev@gmail.com>
2021-12-09 18:49:23 +0530

    [test] Testing CI build

--- conftest.py
+++ conftest.py
@@ -3,5 +3,5 @@ import pytest
 
 @pytest.fixture(scope='session')
 def django_db_modify_db_settings():
-    """ used to speed up pytest with django """
+    """used to speed up pytest with django"""
     pass
