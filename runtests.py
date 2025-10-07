a1ba01de11b500a0e8b02856caf70279c135e61e
Federico Capoano <f.capoano@openwisp.io>
2025-10-07 19:36:27 -0300

    [tests] Allow excluding pytests, updated README

--- runtests.py
+++ runtests.py
@@ -12,7 +12,16 @@ if __name__ == "__main__":
     from django.core.management import execute_from_command_line
 
     args = sys.argv
+    exclude_pytest = "--exclude-pytest" in args
+    if exclude_pytest:
+        args.pop(args.index("--exclude-pytest"))
+
     args.insert(1, "test")
     args.insert(2, "django_loci")
-    execute_from_command_line(args)
-    sys.exit(pytest.main([os.path.join("django_loci", "tests")]))
+    django_tests = execute_from_command_line(args)
+
+    if not exclude_pytest:
+        # pytests is used to test django-channels
+        sys.exit(pytest.main([os.path.join("django_loci", "tests")]))
+    else:
+        sys.exit(django_tests)
