71176daff3e64b7373463fdcb0a4fb5c9b612c5b
dee077 <deepanshu.sahu27@gmail.com>
2025-11-07 17:53:21 +0530

    [fix] Qa

--- django_loci/channels/asgi.py
+++ django_loci/channels/asgi.py
@@ -4,8 +4,11 @@ from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 from django.urls import path
 
-from django_loci.channels.base import location_broadcast_path, all_location_boradcast_path
-from django_loci.channels.consumers import LocationBroadcast, AllLocationBroadcast
+from django_loci.channels.base import (
+    all_location_boradcast_path,
+    location_broadcast_path,
+)
+from django_loci.channels.consumers import AllLocationBroadcast, LocationBroadcast
 
 channel_routing = ProtocolTypeRouter(
     {
@@ -22,7 +25,7 @@ channel_routing = ProtocolTypeRouter(
                             all_location_boradcast_path,
                             AllLocationBroadcast.as_asgi(),
                             name="AllLocationChannel",
-                        )
+                        ),
                     ]
                 )
             )
